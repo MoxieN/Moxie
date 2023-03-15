bdf2834d0eb6b75da9c5eff7068e9b4d83512aad
Kiirx <43090601+Kiirx@users.noreply.github.com>
2023-03-15 22:13:43 +0100

    Base branch commit

---
+++ Moxie_Kernel/Kernel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace Moxie;
+
+public class Kernel : Sys.Kernel
+{
+    //vFS
+    public static string CurrentDirectory = @"0:\";
+    public static string CurrentVolume = @"0:\";
+
+    protected override void BeforeRun()
+    {
+        Console.Clear();
+        Console.WriteLine("Print something to get echoed back");
+    }
+
+    protected override void Run()
+    {
+        Console.WriteLine(Console.ReadLine());
+    }
+}
